00e6b65f4805fd71284b8552d1de669e5cfd31f4
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2022-12-10 00:13:24 -0500

    Section 5 done

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, map } from 'rxjs';
+import { User } from '../_models/User';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccountService {
+  baseUrl = 'https://localhost:5001/api/';
+  private currentUserSource = new BehaviorSubject<User | null>(null);
+  // with the use of $ we denote that this is an observable
+  // When we call the next method on currentUserSource this observable will be fired
+  currentUser$ = this.currentUserSource.asObservable();
+  constructor(private http: HttpClient) {}
+
+  login(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map((user) => {
+        localStorage.setItem('user', JSON.stringify(user));
+        this.currentUserSource.next(user);
+      })
+    );
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+}

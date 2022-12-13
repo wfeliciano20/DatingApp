ede1afa0cfeeebad2d029ca78627a5193d935072
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2022-12-12 21:39:15 -0500

    Section 8 done

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -25,6 +25,10 @@ namespace API.Extensions
 
             services.AddScoped<ITokenService, TokenService>();
 
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
             return services;
         }
     }
---
+++ API/interfaces/IUserRepository.cs
@@ -0,0 +1,22 @@
+using API.DTOs;
+using API.Entities;
+
+namespace API.interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+
+        Task<bool> SaveAllAsyc();
+
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+
+        Task<AppUser> GetUserByIdAsync(int id);
+
+        Task<AppUser> GetUserByUsernameAsync(string username);
+
+        Task<IEnumerable<MemberDTO>> GetMembersAsync();
+
+        Task<MemberDTO> GetMemberAsync(string username);
+    }
+}

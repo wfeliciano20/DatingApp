6f6b089828b295edf6071f46b0dbe12dff7e713f
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2022-12-12 21:58:26 -0500

    Section 8 typo done

--- API/DTOs/MemberDTO.cs
+++ API/DTOs/MemberDTO.cs
@@ -10,7 +10,7 @@ namespace API.DTOs
         public string KnownAs { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
-        public string Gende { get; set; }
+        public string Gender { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string Interest { get; set; }
